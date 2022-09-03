ea66a195aacaccf2b5cb51af5788bad7aba3e3c6
zbt <2479879758@qq.com>
2022-09-04 00:17:28 +0800

    update SideMenue

--- blog-website-manage/src/components/SandBox/SideMenu.tsx
+++ blog-website-manage/src/components/SandBox/SideMenu.tsx
@@ -1,39 +1,124 @@
-import React from 'react'
-import { Layout,Menu } from 'antd'
+import React, { useState } from 'react';
+
+import { Layout, Menu } from 'antd'
+import type { MenuProps } from 'antd';
+import { useNavigate } from 'react-router-dom'
+
 import {
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
 const { Sider } = Layout;
-const items = [
+type MenuItem = Required<MenuProps>['items'][number];
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+const menuData = [
   {
-    key: '1',
+    label: "首页",
+    key: "/sandbox/home",
     icon: <UserOutlined />,
-    label: 'Home',
+    children: [
+      {
+        label: "首页-child",
+        key: "/sandbox/user-manage/role/list",
+        icon: <UserOutlined />,
+      }
+    ]
+
   },
   {
-    key: '2',
-    icon: <VideoCameraOutlined />,
-    label: 'User',
-  },
-  {
-    key: '3',
+    label: "用户管理",
+    key: "/login",
     icon: <UploadOutlined />,
-    label: 'nav 3',
+    children: [
+      {
+        label: "用户管理-child-1",
+        key: "22",
+        icon: <UploadOutlined />,
+        children: [
+          {
+            label: "用户管理-child-2",
+            key: "222",
+            icon: <UploadOutlined />,
+          }
+        ]
+      }
+    ]
+
+  },
+  {
+    label: "视频页",
+    key: "/sandbox/user-manage/role/list",
+    icon: <VideoCameraOutlined />,
+    children: [
+      {
+        label: "主页-child",
+        key: "33",
+        icon: <VideoCameraOutlined />,
+      }
+    ]
+
   },
 ]
 
-function SideMenu() {
+const getMenuList = (List: any) => {
+  let res = [];
+  res = List.map((item: any) => {
+    if (item.children) { // 只遍历了一层
+      item.children = getMenuList(item.children)
+    }
+    return getItem(item.label, item.key, item.icon, item.children, item.type)
+  })
+  return res;
+}
+
+const MenuList: MenuProps['items'] = getMenuList(menuData)
+const rootSubmenuKeys = ['/sandbox/home', '/login', '/sandbox/user-manage/role/list'];
+function SideMenu(props: any) {
+  const navigate = useNavigate();
+
+  const [openKeys, setOpenKeys] = useState(['/sandbox/home']);
+
+  const onOpenChange: MenuProps['onOpenChange'] = keys => {
+    console.log(keys);
+    const latestOpenKey = keys.find(key => openKeys.indexOf(key) === -1);
+    if (rootSubmenuKeys.indexOf(latestOpenKey!) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+  };
+
   return (
-    <Sider>
-       <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            items={items}
-          />
+    <Sider trigger={null} collapsible collapsed={false}>
+      <div className='logo' style={{
+        color: "#fff",
+        fontWeight: "bolder",
+        textAlign: "center",
+        lineHeight: "32px",
+        fontSize: "16px"
+      }}> 全球新闻发布管理系统</div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        openKeys={openKeys}
+        onOpenChange={onOpenChange}
+        items={MenuList}
+      />
     </Sider>
   )
 }
--- blog-website-manage/src/components/SandBox/TopHeader.tsx
+++ blog-website-manage/src/components/SandBox/TopHeader.tsx
@@ -1,15 +1,43 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
+  UserOutlined,
 } from '@ant-design/icons';
 import styles from './sass/TopHeader.module.css'
-import { Layout } from 'antd';
+import { Layout, Dropdown, Menu, Space, Avatar } from 'antd';
 import React, { useState } from 'react';
-const { Header } = Layout;
 
+
+const { Header } = Layout;
+const menu = (
+  <Menu
+    items={[
+      {
+        key: '1',
+        label: (
+          <a target="_blank" rel="noopener noreferrer" >
+            超级管理员
+          </a>
+        ),
+
+      },
+      {
+        key: '2',
+        label: (
+          <a target="_blank" rel="noopener noreferrer">
+            退出登录
+          </a>
+        ),
+
+      }
+    ]}
+  />
+);
 function TopHeader() {
   const [collapsed, setCollapsed] = useState(false);
+    
   return (
     <Header
       className="site-layout-background"
@@ -19,10 +47,28 @@ function TopHeader() {
     >
       {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
         className: styles.trigger,
-        onClick: () => setCollapsed(!collapsed),
+        onClick: () => {
+          setCollapsed(!collapsed)
+        },
       })}
+      <div style={{ float: "right", marginRight: "20px" }}>
+        <span style={{ marginRight: "10px" }}>欢迎admin回来</span>
+        <Dropdown overlay={menu}>
+          <a onClick={e => e.preventDefault()}>
+            <Space>
+              <Avatar size={40} icon={<UserOutlined />} />
+
+            </Space>
+          </a>
+        </Dropdown>
+      </div>
+
       {/* {
-        collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined />
+        collapsed ? <MenuUnfoldOutlined className={styles.trigger} onClick={() => {
+          setCollapsed(!collapsed)
+        }}/> : <MenuFoldOutlined className={styles.trigger}  onClick={() => {
+          setCollapsed(!collapsed)
+        }}/>
       } */}
     </Header>
   )
